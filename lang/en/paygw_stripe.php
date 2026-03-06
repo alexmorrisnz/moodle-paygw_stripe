66505a0865f768fc9dfd8a5a1cac1b1aa8afdd68
Jayce Birrell <jayce88@live.com.au>
2026-03-06 14:56:25 +1030

    Remove unsupported UPI payment method from Stripe gateway
    UPI is not supported by Stripe's payment_method_types parameter.
    Stripe removed support for UPI from PaymentMethodConfiguration in their API.
    
    - Remove UPI from payment methods configuration in gateway.php
    - Remove UPI language string from language file

--- lang/en/paygw_stripe.php
+++ lang/en/paygw_stripe.php
@@ -76,7 +76,6 @@ $string['paymentmethod:nz_bank_account'] = 'NZ BECS Direct Debit';
 $string['paymentmethod:p24'] = 'P24';
 $string['paymentmethod:sepa_debit'] = 'SEPA Direct Debit';
 $string['paymentmethod:twint'] = 'TWINT';
-$string['paymentmethod:upi'] = 'UPI';
 $string['paymentmethod:wechat_pay'] = 'WeChat Pay';
 
 $string['paymentmethods'] = 'Payment Methods';
